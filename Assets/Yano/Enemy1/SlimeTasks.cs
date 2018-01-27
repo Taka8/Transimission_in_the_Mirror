c10307730a5b7a00cfa82c4f700f97feb47d00e1
Takahiro Yano <yano_123@hotmail.com>
2018-01-27 17:47:13 +0900

    Slimeが動くようになった。

---
+++ Assets/Yano/Enemy1/SlimeTasks.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Panda;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class SlimeTasks : MonoBehaviour
+{
+    NavMeshAgent m_Agent;
+
+    void Start()
+    {
+        m_Agent = GetComponent<NavMeshAgent>();
+    }
+
+    [Task]
+    void MoveToPlayer()
+    {
+        GameObject player = GameObject.Find("Player");
+        m_Agent.destination = player.transform.position;
+
+        //rend.material.color = new Color(r, g, b);
+        //Task.current.Succeed(); // <-- Task.current gives access to the run-time task bind to this method.
+    }
+}
---
+++ Assets/Yano/Script/SimpleAnimationController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAnimationController : MonoBehaviour {
+
+    public enum Actions
+    {
+        move = 1,
+        attack = 2,
+        die = 3,
+        damage = 4,
+    }
+
+    SimpleAnimation sa;
+
+	void Start () {
+        sa = GetComponent<SimpleAnimation>();
+	}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            DoAction(Actions.attack);
+        }
+    }
+
+    public void DoAction(Actions action)
+    {
+        sa.Stop();
+        sa.CrossFade(action.ToString(), 0.2f);
+    }
+}
