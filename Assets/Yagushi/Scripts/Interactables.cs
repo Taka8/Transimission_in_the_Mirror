0841222e94c40f8615bce850b2498eb1e56b8ae2
Taka8 <thnx4joiningus@gmail.com>
2018-01-27 13:06:39 +0900

    add some scriptable objects

---
+++ Assets/Yagushi/Scripts/Interactables.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactables : MonoBehaviour
+{
+
+    public bool interactable = false;
+
+    public Material[] material;
+    public Renderer rend;
+
+    void Start()
+    {
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+
+    public virtual void Interact()
+    {
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        if (interactable && Input.GetButtonDown("Fire1"))
+        {
+            Interact();
+        }
+        if (interactable)
+        {
+            rend.sharedMaterial = material[1];
+        }
+        else
+        {
+            rend.sharedMaterial = material[0];
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>())
+        {
+            interactable = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Player>())
+        {
+            interactable = false;
+        }
+    }
+}
---
+++ Assets/Yagushi/Scripts/WeaponItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Inventory/Weapon")]
+public class WeaponItem : MonoBehaviour
+{
+
+    public int damageAmout;
+    public Sprite sprite;
+    public Mesh mesh;
+
+}
